300b14e2ae5aff7b83dd22de119bca8e0f3a5b38
phu333 <58225859+phu333@users.noreply.github.com>
2021-06-24 01:11:04 +0700

    dfgdfg

--- src/pages/OrderDetail.js
+++ src/pages/OrderDetail.js
@@ -1,4 +1,25 @@
 import '../App.css'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+import { lighten, makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TablePagination from '@material-ui/core/TablePagination'
+import TableRow from '@material-ui/core/TableRow'
+import TableSortLabel from '@material-ui/core/TableSortLabel'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import Checkbox from '@material-ui/core/Checkbox'
+import IconButton from '@material-ui/core/IconButton'
+import Tooltip from '@material-ui/core/Tooltip'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Switch from '@material-ui/core/Switch'
+import DeleteIcon from '@material-ui/icons/Delete'
+import FilterListIcon from '@material-ui/icons/FilterList'
 
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
@@ -8,6 +29,8 @@ import { Image, Item, Segment } from 'semantic-ui-react'
 
 function OrderDetail () {
   const { orderId } = useParams()
+  const [historylist, setHistorylist] = useState([])
+
   useEffect(() => {
     axios({
       method: 'GET',
@@ -15,8 +38,231 @@ function OrderDetail () {
     }).then(res => {
       console.log(res)
       console.log(res.data)
+      setHistorylist(res.data)
     })
   }, [orderId])
+  function descendingComparator (a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1
+    }
+    return 0
+  }
+
+  function getComparator (order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy)
+  }
+
+  function stableSort (array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index])
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0])
+      if (order !== 0) return order
+      return a[1] - b[1]
+    })
+    return stabilizedThis.map(el => el[0])
+  }
+
+  const headCells = [
+    {
+      id: 'Id',
+      numeric: false,
+      disablePadding: true,
+      label: 'Id'
+    },
+    { id: 'Name', numeric: false, disablePadding: false, label: 'Name' },
+    {
+      id: 'Quantity',
+      numeric: false,
+      disablePadding: false,
+      label: 'Quantity'
+    },
+    { id: 'Price', numeric: false, disablePadding: false, label: 'Price' }
+  ]
+
+  function EnhancedTableHead (props) {
+    const {
+      classes,
+
+      order,
+      orderBy,
+
+      rowCount,
+      onRequestSort
+    } = props
+    const createSortHandler = property => event => {
+      onRequestSort(event, property)
+    }
+
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map(headCell => (
+            <TableCell
+              key={headCell.id}
+              align={headCell.numeric ? 'right' : 'left'}
+              padding={headCell.disablePadding ? 'none' : 'default'}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <span className={classes.visuallyHidden}>
+                    {order === 'desc'
+                      ? 'sorted descending'
+                      : 'sorted ascending'}
+                  </span>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    )
+  }
+
+  EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+
+    onRequestSort: PropTypes.func.isRequired,
+
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired
+  }
+
+  const useToolbarStyles = makeStyles(theme => ({
+    root: {
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(1)
+    },
+    highlight:
+      theme.palette.type === 'light'
+        ? {
+            color: theme.palette.secondary.main,
+            backgroundColor: lighten(theme.palette.secondary.light, 0.85)
+          }
+        : {
+            color: theme.palette.text.primary,
+            backgroundColor: theme.palette.secondary.dark
+          },
+    title: {
+      flex: '1 1 100%'
+    }
+  }))
+
+  const EnhancedTableToolbar = props => {
+    const classes = useToolbarStyles()
+    const { numSelected } = props
+
+    return (
+      <Toolbar
+        className={clsx(classes.root, {
+          [classes.highlight]: numSelected > 0
+        })}
+      >
+        {numSelected > 0 ? (
+          <Typography
+            className={classes.title}
+            color='inherit'
+            variant='subtitle1'
+            component='div'
+          >
+            {numSelected} selected
+          </Typography>
+        ) : (
+          <Typography
+            className={classes.title}
+            variant='h6'
+            id='tableTitle'
+            component='div'
+          >
+            <Title Name='User addresses' />
+          </Typography>
+        )}
+
+        {numSelected > 0 ? (
+          <Tooltip title='Delete'>
+            <IconButton aria-label='delete'>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title='Filter list'>
+            <IconButton aria-label='filter list'>
+              <FilterListIcon />
+            </IconButton>
+          </Tooltip>
+        )}
+      </Toolbar>
+    )
+  }
+
+  EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired
+  }
+
+  const useStyles = makeStyles(theme => ({
+    root: {
+      width: '100%'
+    },
+    paper: {
+      width: '100%',
+      marginBottom: theme.spacing(2)
+    },
+    table: {
+      minWidth: 750
+    },
+    visuallyHidden: {
+      border: 0,
+      clip: 'rect(0 0 0 0)',
+      height: 1,
+      margin: -1,
+      overflow: 'hidden',
+      padding: 0,
+      position: 'absolute',
+      top: 20,
+      width: 1
+    }
+  }))
+  const classes = useStyles()
+  const [order, setOrder] = useState('asc')
+  const [orderBy, setOrderBy] = useState('calories')
+  const [selected, setSelected] = useState([])
+  const [page, setPage] = useState(0)
+  // const [dense, setDense] = useState(false)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc'
+    setOrder(isAsc ? 'desc' : 'asc')
+    setOrderBy(property)
+  }
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
+
+  // const handleChangeDense = event => {
+  //   setDense(event.target.checked)
+  // }
+
+  const isSelected = name => selected.indexOf(name) !== -1
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, historylist.length - page * rowsPerPage)
 
   return (
     <>
@@ -29,22 +275,99 @@ function OrderDetail () {
         </Steps>
       </Segment>
       <Segment vertical>
-        <div>
-          <Item>
-            <Item.Image
-              size='tiny'
-              src='https://react.semantic-ui.com/images/wireframe/image.png'
-            />
+        ;
+        <div className={classes.root}>
+          <Paper className={classes.paper}>
+            <EnhancedTableToolbar numSelected={selected.length} />
+            <TableContainer>
+              <Table
+                className={classes.table}
+                aria-labelledby='tableTitle'
+                size={'medium'}
+                aria-label='enhanced table'
+              >
+                <EnhancedTableHead
+                  classes={classes}
+                  numSelected={selected.length}
+                  order={order}
+                  orderBy={orderBy}
+                  onRequestSort={handleRequestSort}
+                  rowCount={historylist.length}
+                />
+                <TableBody>
+                  {stableSort(historylist, getComparator(order, orderBy))
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row, index) => {
+                      const isItemSelected = isSelected(row.Name)
+                      const labelId = `enhanced-table-checkbox-${index}`
 
-            <Item.Content>
-              <Item.Header as='a'>Header</Item.Header>
-              <Item.Meta>Description</Item.Meta>
-              <Item.Description>
-                <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
-              </Item.Description>
-              <Item.Extra>Additional Details</Item.Extra>
-            </Item.Content>
-          </Item>
+                      return (
+                        <TableRow
+                          hover
+                          onClick={event => handleClick(event, row.name)}
+                          role='checkbox'
+                          aria-checked={isItemSelected}
+                          tabIndex={-1}
+                          key={row.name}
+                          selected={isItemSelected}
+                        >
+                          <TableCell component='th' scope='row'>
+                            {row.Name}
+                          </TableCell>
+                          <TableCell align='right'>{row.Quantity}</TableCell>
+                          <TableCell align='right'>
+                            {row.Price}
+                          </TableCell>
+                          {/* <TableCell align='right'>
+                            {row.AddressShipping}
+                          </TableCell>
+                          <TableCell align='right'>{row.TotalPrice}</TableCell>
+                          <TableCell align='right'>{row.Status}</TableCell> */}
+                        </TableRow>
+                      )
+                    })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                  <TableRow>
+                    <TableCell rowSpan={3} />
+                    <TableCell colSpan={2}>Subtotal</TableCell>
+                    <TableCell align='right'>
+                      {/* {ccyFormat(invoiceSubtotal)} */}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Tax</TableCell>
+                    <TableCell align='right'>
+                      {/* {`${(TAX_RATE * 100).toFixed(
+                      0
+                    )} %`} */}
+                    </TableCell>
+                    <TableCell align='right'>
+                      {/* {ccyFormat(invoiceTaxes)} */}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell colSpan={2}>Total</TableCell>
+                    <TableCell align='right'>
+                      {/* {ccyFormat(invoiceTotal)} */}
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component='div'
+              count={historylist.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+          </Paper>
         </div>
       </Segment>
     </>
